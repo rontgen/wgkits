8c10fdf2fa8e61cf5ac484522bb9cee792ebe95a
rontgen <minstrelgen@163.com>
2018-07-24 21:02:48 +0800

    [modify version]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wgkits",
-    version="0.0.6",
+    version="0.0.7",
     author="rontgen",
     author_email="rontgen@pku.edu.cn",
     description="Android build apk tools",
