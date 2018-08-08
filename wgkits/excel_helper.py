a94ecd245faa764e5dc5625cbb01b37b265cb901
rontgen <minstrelgen@163.com>
2018-08-08 18:11:59 +0800

    add excel_helper

---
+++ wgkits/excel_helper.py
@@ -0,0 +1,9 @@
+#-*-:coding utf-8 -*
+
+"""
+author:rontgen(wanggeng)
+Date: 2018-08-08
+Function Description:
+    1. read, write, add, del, modify, query operation for excel files
+    2. read data from excel, analyze and draw graphic chart
+"""
